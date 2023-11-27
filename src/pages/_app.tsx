614db6d178e710ce9c7a35a98b706e3252e40f1c
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-11-27 19:28:43 +0100

    font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="">
+    <div className="font-quicksand">
       <Component {...pageProps} />
     </div>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -52,3 +52,5 @@ const index = () => {
 };
 
 export default index;
+
+
